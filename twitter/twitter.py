8648a4c9c03ef2e15b1efa3f1fc4938e5571f7c1
Osmir Mariano <osmirmariano@uft.edu.br>
2020-09-14 07:47:55 -0300

    Integrando com twitter

--- twitter/twitter.py
+++ twitter/twitter.py
@@ -1,22 +1,26 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_TOKEN_KEY= ''
+ACCESS_TOKEN_SECRET= ''
 
 import twitter
 
 def auth():
     api = twitter.Api(
-        consumer_key='',
-        consumer_secret='',
-        access_token='',
-        access_token_secret=''
+        consumer_key=CONSUMER_KEY,
+        consumer_secret=CONSUMER_SECRET,
+        access_token=ACCESS_TOKEN_KEY,
+        access_token_secret=ACCESS_TOKEN_SECRET
     )
+    
     return api
 
-def getMessage():
+def get_message():
     api = auth()
-    response = api.GetSearch(term='')
+    response = api.GetSearch(term='CBLoL')
     for i in response:
         print(i)
 
 if __name__ == '__main__':
-    getMessage()
+    get_message()
