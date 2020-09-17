c9b9e39f3638e562bae3dd5dad95f9894d23e423
Osmir Mariano <osmirmariano@uft.edu.br>
2020-09-16 22:27:04 -0300

    Consumindo api do twitter e pegando os dados e salvando em json

--- twitter/twitter.py
+++ twitter/twitter.py
@@ -1,26 +1,45 @@
 # -*- coding: utf-8 -*
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
-ACCESS_TOKEN_KEY= ''
-ACCESS_TOKEN_SECRET= ''
 
-import twitter
+import tweepy as tw
+import pandas as pd
 
 def auth():
-    api = twitter.Api(
-        consumer_key=CONSUMER_KEY,
-        consumer_secret=CONSUMER_SECRET,
-        access_token=ACCESS_TOKEN_KEY,
-        access_token_secret=ACCESS_TOKEN_SECRET
-    )
-    
+    with open('file/tokens.txt', 'r') as tfile:
+        consumer_key = tfile.readline().strip('\n')
+        consumer_secret = tfile.readline().strip('\n')
+        access_token = tfile.readline().strip('\n')
+        access_token_secret = tfile.readline().strip('\n')
+        
+    auth_user = tw.OAuthHandler(consumer_key, consumer_secret)
+    auth_user.set_access_token(access_token, access_token_secret)
+
+    api = tw.API(auth_user)
     return api
 
-def get_message():
+def getMessage():
     api = auth()
-    response = api.GetSearch(term='CBLoL')
-    for i in response:
-        print(i)
+    
+    querySearch = '#CORONAVIRUS #COVID19 ' + '-filter:retweet'
+    tweets = tw.Cursor(api.search, q=querySearch).items(100)
+    
+    # Creating directory
+    tweetsDict = {}
+    twkey = {'text': None, 'created_at': None, 'entities': None, 'lang': None}
+    tweetsDict = tweetsDict.fromkeys(twkey)
+    
+    for tweet in tweets:    
+        for key in tweetsDict.keys():
+            try:
+                twKey = tweet._json[key]
+                tweetsDict[key].append(twKey)
+            except KeyError:
+                twKey = ""
+                tweetsDict[key].append("")
+            except:
+                tweetsDict[key] = [twKey]
+    dfTweets = pd.DataFrame.from_dict(tweetsDict)
+    dfTweets.head()
+    dfTweets.to_json('file/result.json')
 
 if __name__ == '__main__':
-    get_message()
+    getMessage()
