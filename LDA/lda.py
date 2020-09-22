fa59077228bb533dd421b4779825cc28ec8d21e7
Osmir Mariano <osmirmariano@uft.edu.br>
2020-09-21 22:38:35 -0300

    Tratamento de erros

--- LDA/lda.py
+++ LDA/lda.py
@@ -6,13 +6,18 @@ from stop_words import get_stop_words
 from nltk.stem.porter import PorterStemmer
 from gensim import corpora, models
 import gensim
+import nltk
 import pandas as pd
 import json
+
+import re
+
 
 tokenizer = RegexpTokenizer(r'\w+')
 
 # create English stop words list
-enStop = get_stop_words('en')
+stop_words = get_stop_words('portuguese', cache=True)
+stop_words += get_stop_words('english', cache=True)
 
 # Create pStem of class PorterStemmer
 pStem = PorterStemmer()
@@ -28,22 +33,21 @@ def modeling():
     texts = []
 
     for i in data:
-        raw = data[i].lower()
-        tokens = tokenizer.tokenize(raw)
-
-        tokenStop = [i for i in tokens if not i in enStop]
-        
-        tokenStem = [pStem.stem(i) for i in tokenStop]
-
-        texts.append(tokenStem)
-    
+        try:
+            raw = data[i].lower()
+            tokens = tokenizer.tokenize(raw)
+            
+            tokenStop = [i for i in tokens if not i in stop_words]
+            tokenStem = [pStem.stem(i) for i in tokenStop]
+            texts.append(tokenStem)
+        except:
+            continue
     ldaModel(texts)
     
-
 def ldaModel(texts):
     dictionary = corpora.Dictionary(texts)
     corpus = [dictionary.doc2bow(text) for text in texts]
-    ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=2, id2word = dictionary, passes=20)
+    ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=10, id2word = dictionary, passes=1)
     print(ldamodel.print_topics(num_topics=2, num_words=4))
 
 if __name__ == "__main__":
